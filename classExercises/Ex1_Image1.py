7a28341f69d509f6fb5eda83408490a66ed16571
cristiantirado <157725944+cutiradom@users.noreply.github.com>
2024-05-24 12:10:52 -0500

    Add files via upload

---
+++ classExercises/Ex1_Image1.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+W=500
+H=500
+
+im = np.zeros ((W,H), np.uint8)
+
+def show_im(im):
+    cv2.imshow('window', im)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+for i in range (W): im[i,i] = (255)
+
+show_im(im)
